0185ebd66aa61592077477d465cd0e16b994eec3
Antonio Torralba <4169402@alu.murciaeduca.es>
2018-05-14 09:40:32 +0200

    Esquelo inicial de la aplicacion de la gention de base de datos en Java

---
+++ src/mediamarkt/Mediamarkt.java
@@ -0,0 +1,46 @@
+
+package mediamarkt;
+
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Mediamarkt {
+
+    public static void cargaDatos(){
+        try {
+            //1.Crea Driver
+            Class.forName("com.mysql.jdbc.Driver");
+            
+            //2.Creo la conexion contra ña BD
+            Connection conexion=
+                    DriverManager.getConnection("jdbc://localhost/mediamarkt","root","");
+            
+            //3.Crear el Statament
+            Statement s=conexion.createStatement();
+            
+            //4. Trabajo con las consultas
+            ResultSet rs=s.executeQuery("SELECT* FROM PRODUCTOS");
+            while(rs.next()){
+                System.out.println(rs.getInt("id")+" "+
+                        rs.getString("nombre")+" "+
+                        rs.getFloat("precio"));
+            }
+            
+            //5.Cerrar Conexion
+            rs.close();
+            s.close();
+            conexion.close();
+            
+        } catch (Exception ex) {
+            System.out.println("Error al acceder a la base de datos");
+            //Muestra la informacion del error
+            ex.printStackTrace();
+        }
+    }
+    
+    public static void main(String[] args) {
+        cargaDatos();
+    }
+    
+}
